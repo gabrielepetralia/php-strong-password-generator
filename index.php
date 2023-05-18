16c5c71ff6f2daec470cbacfaade37461839f259
gabrielepetralia <gabri.petralia03@gmail.com>
2023-05-18 09:54:59 +0200

    code optimization using a min value in input tag

--- index.php
+++ index.php
@@ -2,23 +2,15 @@
 require_once __DIR__ . "/partials/functions.php";
 
 $password = "";
-$error_msg = "";
-$old_pwd_length = 0;
+$old_pwd_length = 4;
 
 if (isset($_GET['pwd_length'])) {
   $pwd_length = $_GET['pwd_length'];
-  if ($_GET['pwd_length'] >= 4) {
-    $password = generatePassword($pwd_length);
-    $old_pwd_length = $pwd_length;
-    $error_msg = "";
+  $old_pwd_length = $pwd_length;
 
-    session_start();
-    $_SESSION['pwd'] = $password;
-    header('Location: ./pwd.php');
-  } else {
-    $old_pwd_length = $pwd_length;
-    $error_msg = "Lunghezza minima: 4 caratteri!";
-  }
+  session_start();
+  $_SESSION['pwd'] = generatePassword($pwd_length);
+  header('Location: ./pwd.php');
 }
 ?>
 
@@ -44,12 +36,9 @@ require_once __DIR__ . "/partials/head.html"
 
             <h5 class="card-title mb-0">Password Length:</h5>
             <form action="index.php" method="GET" class="mb-0">
-              <input class="my-2 ps-2" type="number" value="<?php echo $old_pwd_length ?>" placeholder="0" name="pwd_length">
-              <span class="text-danger d-block mb-2"><?php echo $error_msg ?></span>
+              <input class="my-2 ps-2" type="number" min="4" value="<?php echo $old_pwd_length ?>" placeholder="0" name="pwd_length">
               <button type="submit" class="btn btn-warning fw-semibold">Generate Password</button>
             </form>
-            <!-- <h5 class="card-title mb-2">Generated Password:</h5>
-            <span><?php echo $password ?></span> -->
 
           </div>
         </div>
